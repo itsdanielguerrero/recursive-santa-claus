5f9f915910c7ee06d4a1c34cc73b47efb5b2981d
itsdanielguerrero <itsdanielguerrero@gmail.com>
2019-09-25 20:52:26 -0400

    Reworked deliverPresents module to be recursive

--- santa.js
+++ santa.js
@@ -1,5 +1,13 @@
 function deliverPresents(houses) {
-  console.log('Delivering presents to ' + houses[0])
+  if (houses.length === 1){ //base case - if length is one print last name on list
+    console.log('Delivering presents to ' + houses[0])
+    return 0
+  }
+
+  var nextKid = houses[0] //grab next kid off of the list
+  var restOfKids = houses.slice(1, houses.length) //store rest of kids on the list
+  console.log('Delivering presents to ' + nextKid)
+  deliverPresents(restOfKids) //recursive call
 }
 
 module.exports = deliverPresents
